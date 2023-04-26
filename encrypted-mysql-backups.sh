bbaec6c50c6a77983a8efa5bd3e9195e28ca15f0
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-04-25 21:25:56 -0700

    add recurring backups

--- encrypted-mysql-backups.sh
+++ encrypted-mysql-backups.sh
@@ -100,8 +100,6 @@ perform_backup
 echo "
 Your backup directory is located at ${BACKUP_DIR}
 "
-<<<<<<< Updated upstream:encrypted-mysql-backups.sh
-=======
 
 # Create a new script with the backup function and necessary variables
 BACKUP_SCRIPT="${BACKUP_DIR}/daily_backup.sh"
@@ -125,4 +123,3 @@ chmod +x "${BACKUP_SCRIPT}"
 
 # Schedule the new script to run daily at midnight using cron
 (crontab -l 2>/dev/null; echo "0 0 * * * ${BACKUP_SCRIPT} >> ${BACKUP_DIR}/logfile.log 2>&1") | crontab -
->>>>>>> Stashed changes:backup-mediawiki.sh
