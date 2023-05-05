3afbfd4139242c826bb12f9f40eb9c78162b4821
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-05-04 22:09:13 -0700

    Update hushline-eink-display.sh

--- hushline-eink-display.sh
+++ hushline-eink-display.sh
@@ -52,8 +52,8 @@ def display_status(epd, status, onion_address):
     font_onion = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 12)
 
     x_pos_status = 10
-    draw.text((0, 0), status, font=font_status, fill=0)
-
+    draw.text((x_pos_status, 0), status, font=font_status, fill=0)
+    
     # Wrap the onion address to fit the display width
     max_width = epd.height
     chars_per_line = max_width // font_onion.getsize('A')[0]
