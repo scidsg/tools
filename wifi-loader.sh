f73bb490f5a8554e3edf111175d0ed6c2be858f0
Glenn Sorrentino <glenn.sorrentino@proton.me>
2023-05-21 14:34:28 -0700

    Update wifi-loader.sh

--- wifi-loader.sh
+++ wifi-loader.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+# Welcome message and ASCII art
+cat << "EOF"
+ __      __  _    __   _     _                      _             
+ \ \    / / (_)  / _| (_)   | |     ___   __ _   __| |  ___   _ _ 
+  \ \/\/ /  | | |  _| | |   | |__  / _ \ / _` | / _` | / -_) | '_|
+   \_/\_/   |_| |_|   |_|   |____| \___/ \__,_| \__,_| \___| |_|  
+
+A free tool by Science & Design - https://scidsg.org
+Bulk-add trusted wifi networks to your device. 
+
+EOF
+
 # Path to wpa_supplicant.conf
 WPA_SUPPLICANT_CONF="/etc/wpa_supplicant/wpa_supplicant.conf"
 
-# Function to get network information
-get_network_info() {
-    SSID=$(whiptail --inputbox "Enter the SSID" 8 78 --title "Network Information" 3>&1 1>&2 2>&3)
-    while true; do
-        PASSWORD=$(whiptail --passwordbox "Enter the Wi-Fi Password" 8 78 --title "Network Information" 3>&1 1>&2 2>&3)
-        if [ ${#PASSWORD} -ge 8 ]; then
-            break
-        else
-            whiptail --title "Invalid Password" --msgbox "Password should be at least 8 characters long!" 8 45
-        fi
-    done
-}
+# Check if wpa_supplicant.conf exists
+if [[ ! -f $WPA_SUPPLICANT_CONF ]]; then
+    whiptail --title "File Not Found" --msgbox "The wpa_supplicant.conf file was not found. This could be because the system does not require Wi-Fi connectivity, is networked virtually, or is connected via an Ethernet cable. If Wi-Fi is needed, please ensure the necessary Wi-Fi software is installed." 10 80
+    exit 1
+fi
 
 # Function to check if network already exists
 check_network_exists() {
-    grep -q "id_str=\"$(echo -n "$SSID" | sha256sum | cut -d " " -f 1)\"" $WPA_SUPPLICANT_CONF
+    grep -qP 'ssid="'$(echo "$SSID" | sed 's/[^a-zA-Z0-9]/\\&/g')'"' $WPA_SUPPLICANT_CONF
 }
 
 # Function to add network to wpa_supplicant.conf
@@ -27,41 +32,49 @@ add_network() {
     HASHED_PSK=$(wpa_passphrase "$SSID" "$PASSWORD" | grep 'psk=' | tail -n 1)
 
     # Prepare network block
-    NETWORK_BLOCK="\nnetwork={
-        ssid=\"$(printf '%q' "$SSID")\"
+    NETWORK_BLOCK="
+network={
+    ssid=\"$(printf '%q' "$SSID")\"
 $HASHED_PSK
-        id_str=\"$(echo -n "$SSID" | sha256sum | cut -d " " -f 1)\"
-        key_mgmt=WPA-PSK
-}\n"
+    key_mgmt=WPA-PSK
+}"
 
     # Append network block to wpa_supplicant.conf
-    echo -e "$NETWORK_BLOCK" >> $WPA_SUPPLICANT_CONF
+    echo "$NETWORK_BLOCK" >> $WPA_SUPPLICANT_CONF
 }
 
 # Function to overwrite network in wpa_supplicant.conf
 overwrite_network() {
     # Remove the existing network block
-    sed -i "/id_str=\"$(echo -n "$SSID" | sha256sum | cut -d " " -f 1)\"/,/}/d" $WPA_SUPPLICANT_CONF
+    sed -i "/ssid=\"$(printf '%q' "$SSID")\"/,/}/d" $WPA_SUPPLICANT_CONF
 
     # Add the new network
     add_network
 }
 
-while true; do
-    if (whiptail --title "Add Wi-Fi Network" --yes-button "Add" --no-button "Quit" --yesno "Do you want to add a Wi-Fi network?" 10 60) then
-        get_network_info
-        if check_network_exists; then
-            if (whiptail --title "Network Exists" --yes-button "Overwrite" --no-button "Skip" --yesno "Network already exists. Do you want to overwrite it?" 10 60) then
-                overwrite_network
-                whiptail --title "Add Wi-Fi Network" --msgbox "Network overwritten successfully!" 8 45
-            else
-                whiptail --title "Add Wi-Fi Network" --msgbox "Skipped network!" 8 45
-            fi
-        else
-            add_network
-            whiptail --title "Add Wi-Fi Network" --msgbox "Network added successfully!" 8 45
-        fi
+# Function to process a line (network) from the input file
+process_network() {
+    SSID=$1
+    PASSWORD=$2
+
+    if check_network_exists; then
+        echo "Network $SSID already exists. Overwriting."
+        overwrite_network
     else
-        break
+        add_network
+        echo "Network $SSID added successfully!"
     fi
-done
+}
+
+# Check if a file was provided
+if [[ ! -f $1 ]]; then
+    echo "Please provide a valid file as input."
+    exit 1
+fi
+
+# Process each line in the input file
+while read -r line; do
+    process_network $line
+done < "$1"
+
+echo "All networks processed successfully!"
